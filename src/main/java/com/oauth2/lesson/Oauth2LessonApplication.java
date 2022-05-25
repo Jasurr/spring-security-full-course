649c53f00d00be5297156bd4d4ab4e9edb0949f6
JasurNishonov <ni$h0n0v>
2022-05-25 15:41:11 +0500

    spring security learning from Amigos Code

---
+++ src/main/java/com/oauth2/lesson/Oauth2LessonApplication.java
@@ -0,0 +1,13 @@
+package com.oauth2.lesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2LessonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2LessonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oauth2/lesson/Oauth2LessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth2.lesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2LessonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
